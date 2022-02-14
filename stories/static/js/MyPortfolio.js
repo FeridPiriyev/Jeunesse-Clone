733522a383426bc25ead0a3b30217e7e7b0b1cd7
FeridPiriyev <smdpiriyev@gmail.com>
2022-02-14 17:22:49 +0400

    complated

---
+++ stories/static/js/MyPortfolio.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector(".fas.fa-bars")
+let menu = document.querySelector(".menuyan")
+burger.addEventListener("click", ()=>{
+    menu.classList.toggle("active")
+})
+
+
